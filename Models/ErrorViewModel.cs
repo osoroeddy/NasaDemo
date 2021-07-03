0fe6fcfc975160883cbe76b0e2b5e956a7d247f9
osoroeddy <osoroeddy@hotmail.com>
2021-07-03 10:38:59 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NasaImagesDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
