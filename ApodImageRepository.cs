e2ed6f6c01e75e07278a33f17b8e6776964bd30e
osoroeddy <osoroeddy@hotmail.com>
2021-07-05 11:18:16 -0400

    Additional

---
+++ ApodImageRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NasaImagesDemo
+{
+    public class ApodImageRepository
+    {
+        //public List<ApodImage> GetApodImages()
+        //{
+        //    NasaImagesDBContext imagesDBContext = new NasaImagesDBContext();
+        //    return imagesDBContext.
+
+        //}
+    }
+}
---
+++ dbNasaImagesDemo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NasaImagesDemo
+{
+    public partial class dbNasaImagesDemo : DbContext
+    {
+        public virtual DbSet<ApodImage> ApodImages {get; set;}
+    }
+}
