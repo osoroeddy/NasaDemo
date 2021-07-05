f43f14518c08c28099d937a1b14a47e1a71ab866
osoroeddy <osoroeddy@hotmail.com>
2021-07-05 09:34:34 -0400

    Additional code

---
+++ ApodImageCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NasaImagesDemo.ViewModels
+{
+    public class ApodImageCreateViewModel
+    {
+        public string copyright { get; set; }
+        public string date { get; set; }
+        public string explanation { get; set; }
+        public string hdurl { get; set; }
+        public string media_type { get; set; }
+        public string service_version { get; set; }
+        public string title { get; set; }
+        public IFormFile Imageurl { get; set; }
+    }
+}
---
+++ NasaImagesDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace NasaImagesDemo
+{
+    public class NasaImagesDBContext : DbContext
+    {
+         DbSet<ApodImage> ApodImages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\sqlexpress;Initial Catalog=ApodImageDB;Integrated Security=True");
+
+        }
+    }
+}
