eeb44f25f75e9ac0c3413c868be244ad2fc9861b
osoroeddy <osoroeddy@hotmail.com>
2021-07-05 14:18:04 -0400

    Additions

---
+++ Migrations/20210705180301_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NasaImagesDemo.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApodImages",
+                columns: table => new
+                {
+                    ApodImageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    copyright = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    explanation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    hdurl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    media_type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    service_version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    url = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApodImages", x => x.ApodImageID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApodImages");
+        }
+    }
+}
