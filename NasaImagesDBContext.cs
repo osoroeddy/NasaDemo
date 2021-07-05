af17a07b9d0c9e4855ebdd2f50ddd09acbf86624
osoroeddy <osoroeddy@hotmail.com>
2021-07-05 14:14:28 -0400

    Additions

--- NasaImagesDBContext.cs
+++ NasaImagesDBContext.cs
@@ -5,12 +5,25 @@ namespace NasaImagesDemo
 {
     public class NasaImagesDBContext : DbContext
     {
-         DbSet<ApodImage> ApodImages { get; set; }
+        public NasaImagesDBContext()
+        {
+        }
+
+        public NasaImagesDBContext(DbContextOptions<NasaImagesDBContext> options)
+            : base(options)
+        { 
+                
+        }
+
+
+        DbSet<ApodImage> ApodImages { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=.\sqlexpress;Initial Catalog=ApodImageDB;Integrated Security=True");
 
         }
+
+
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,9 +13,10 @@ namespace NasaImagesDemo
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
         public IConfiguration Configuration { get; }
@@ -23,6 +25,8 @@ namespace NasaImagesDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<NasaImagesDBContext>(options =>options.UseSqlServer(_config.GetConnectionString("NasaDBConnection")));
+            services.AddTransient<ApodImageRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
